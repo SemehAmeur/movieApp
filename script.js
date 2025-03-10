339ebebc46069998c2ae526cdeec1775d1713b80
SemehAmeur <semeh.ameur@gmail.com>
2025-03-10 03:39:10 +0100

    feat: Adding the dom

--- script.js
+++ script.js
@@ -6,17 +6,52 @@ const searchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=b9c91cad7cf
 //get elements
 const formEl = document.getElementById('form');
 const searchEl = document.getElementById('search');
+const mainEl = document.getElementById('main');
+
 //get movies
 getMovies(apiUrl)
+
+//showMovies function
+const showMovies = (movies) =>{
+    mainEl.innerHTML = '';
+    movies.forEach(element => {
+        const { title, poster_path, vote_average, overview} = element;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img
+          src="${imgPath + poster_path + '"'}"
+          alt=""
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>overview</h3>
+          ${overview}
+        </div>
+        `;
+        mainEl.appendChild(movieEl);
+    });
+}
+
+//getClassByRate function to get the color based on the rating
+const getClassByRate = (vote) => {
+    if(vote >= 8) return 'green';
+    else if(vote >= 5) return 'orange';
+    else return 'red';
+}
 
 //getMovies function
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data)
+    showMovies(data.results)
 }
 
+//search functionality
 formEl.addEventListener('submit', e =>{
     e.preventDefault();
     const searchMovie = searchEl.value;
